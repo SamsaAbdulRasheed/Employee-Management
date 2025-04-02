ab147918eb25512eda474394102263c67d4917d6
samsa <samsarasheedmp@gmail.com>
2025-04-02 00:50:56 -0500

    start

---
+++ src/main/java/com/employee/Employee_managment/EmployeeManagmentApplication.java
@@ -0,0 +1,15 @@
+package com.employee.Employee_managment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManagmentApplication.class, args);
+System.out.println("hello world");
+
+	}
+
+}
